eb2eb1269f76afaf7adac5d01da7ab4e222faf74
dpdiaz <90003779+dpdiaz@users.noreply.github.com>
2021-09-14 11:04:23 -0500

    v1 Ajuste de swagger con basic auth

--- ms-products/src/main/java/com/prototype/product/auth/BasicAuthenticationConfig.java
+++ ms-products/src/main/java/com/prototype/product/auth/BasicAuthenticationConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -26,15 +28,28 @@ public class BasicAuthenticationConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
 		auth.inMemoryAuthentication().withUser(user).password(passwordEncoder().encode(password)).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.anyRequest()
+		        .authenticated().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().httpBasic()
+				.authenticationEntryPoint(authenticationEntryPoint);
 
-		http.csrf().disable().httpBasic().authenticationEntryPoint(authenticationEntryPoint).and().authorizeRequests()
-				.anyRequest().authenticated();
+
+	}
+	@Override
+	public void configure(final WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs",
+				"/configuration/ui",
+				"/swagger-resources/**",
+				"/webjars/**",
+				"/configuration/security",
+				"/swagger-ui.html");
 	}
 
 	@Bean
